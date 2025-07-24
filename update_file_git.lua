d3ab06c53b0069f451460f9505c75f229d9b4fd0
JDParker714 <jdparker.robotics@gmail.com>
2025-07-24 00:03:49 -0700

    fix wget

--- update_file_git.lua
+++ update_file_git.lua
@@ -1,9 +1,9 @@
 local files = {
-    { name = "master_server.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/refs/heads/main/master_server.lua" },
-    { name = "player_card_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/refs/heads/main/player_card_kiosk.lua" },
-    { name = "sell_shop.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/refs/heads/main/sell_shop.lua" },
-    { name = "transfer_balance_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/refs/heads/main/transfer_balance_kiosk.lua" },
-    { name = "purchase_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/refs/heads/main/purchase_kiosk.lua" }
+    { name = "master_server.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/main/master_server.lua" },
+    { name = "player_card_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/main/player_card_kiosk.lua" },
+    { name = "sell_shop.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/main/sell_shop.lua" },
+    { name = "transfer_balance_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/main/transfer_balance_kiosk.lua" },
+    { name = "purchase_kiosk.lua", url = "https://raw.githubusercontent.com/JDParker714/CC-MMO/main/purchase_kiosk.lua" }
 }
 
 for _, file in ipairs(files) do
