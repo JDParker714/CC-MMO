2c8c1d5182ff4aaac3201655ba0070f82072aee4
JDParker714 <jdparker.robotics@gmail.com>
2025-07-25 09:16:28 -0700

    coal v3

--- coal_stealer.lua
+++ coal_stealer.lua
@@ -12,30 +12,38 @@ end
 
 -- === MAIN LOOP ===
 while true do
-  -- Step 1: Grab all non-coal items from the chest in front
-  for slot = 1, 16 do
-    turtle.select(slot)
-    turtle.suck()
-    local item = turtle.getItemDetail()
-    if is_coal(item) then
-      -- Put coal back into chest if accidentally sucked
-      turtle.drop()
+  local chest = peripheral.wrap("front")
+  if not chest then
+    print("No chest in front!")
+    sleep(sleep_seconds)
+  else
+    local items = chest.list()
+    for chest_slot, item in pairs(items) do
+      if not is_coal(item) then
+        -- Find a free turtle slot
+        for turtle_slot = 1, 16 do
+          if turtle.getItemCount(turtle_slot) == 0 then
+            turtle.select(turtle_slot)
+            chest.pullItems("front", chest_slot, item.count)
+            break
+          end
+        end
+      end
     end
   end
 
-  -- Step 2: Turn around and deposit non-coal items
+  -- Turn and drop non-coal items
   turtle.turnLeft()
   turtle.turnLeft()
-  for slot = 1, 16 do
-    turtle.select(slot)
+  for i = 1, 16 do
+    turtle.select(i)
     local item = turtle.getItemDetail()
     if item and not is_coal(item) then
       turtle.drop()
     end
   end
+  turtle.turnLeft()
+  turtle.turnLeft()
 
-  -- Step 3: Face forward again and wait
-  turtle.turnLeft()
-  turtle.turnLeft()
   sleep(sleep_seconds)
 end
